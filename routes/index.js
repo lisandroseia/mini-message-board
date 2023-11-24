f94af731b7924bff7e8d09ac1e3b7e13d094a361
lisandro seia <lisandroseia11@gmail.com>
2023-11-24 11:53:08 -0300

    Finish serverless version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini messageboard",messages: messages });
 });
+
+router.get("/new", (req, res, next) =>{
+  res.render("form", {title: "Add your message"})
+})
+
+router.post("/new", (req, res, next) => {
+
+  const {userInput, textInput} = req.body
+  const obj = { user: userInput, text: textInput, added: new Date() }
+  messages.push(obj)
+  res.redirect("/")
+})
+
+
 
 module.exports = router;
